133306a5f64102a41939b317fdfcd1a0d751436d
Sebastiaan Hekner <sebastiaan.hekner@secrid.com>
2022-02-10 15:08:01 +0100

    Minor updates

--- src/consumer.ts
+++ src/consumer.ts
@@ -129,7 +129,6 @@ export class RedisConsumer<S extends RedisScripts> {
 
     const streamsMessages = await this.readStreams(streamsToListen);
     if (!streamsMessages) {
-      console.log('No messages returned from client');
       await this.acknowlegdeMessages();
       this.listenForStreams();
       return;
